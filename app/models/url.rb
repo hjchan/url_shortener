532baa29c2b53a0fd1155062d0e2eccae47baf35
hjchan <jasonlohch@gmail.com>
2017-09-05 14:39:30 +0800

    done

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,12 @@
 class Url < ApplicationRecord
+
+	validates :long_url, 
+		presence: true,
+		format: { with: /\Ahttp:\/\//,
+				 	message: "Wrong format"}
+	
+	def shorten
+		new_url = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+		self[:short_url] = new_url
+	end
 end
